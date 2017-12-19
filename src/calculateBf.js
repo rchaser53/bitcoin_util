153b75c9e8630a224a1075b76a9cbdf73c103669
rchaser53 <tayoshizawa29@gmail.com>
2017-12-19 23:54:31 +0900

    try to use .prettierignore and .prettierrc

--- src/calculateBf.js
+++ src/calculateBf.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-  convertBfToCC: (history) => {
-    return history.map((ret) => {
+  convertBfToCC: history => {
+    return history.map(ret => {
       return {
-        type: (ret.side === 'SELL') ? 'sell' : 'buy',
+        type: ret.side === 'SELL' ? 'sell' : 'buy',
         amount: ret.size,
         spendMoney: ret.size * ret.price,
-        rate: ret.price
+        rate: ret.price,
       }
     })
-  }
+  },
 }
--- src/main.js
+++ src/main.js
@@ -1,29 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+
 const mathjs = require('mathjs')
 
-const history = [
+const { convertBfToCC } = require('./calculateBf')
+
+const unRegistedData = [
   {
     type: 'sell',
-    amount: 1550.0,
-    spendMoney: 49887,
-    rate: 32.18516129
+    rate: 2100000,
+    amount: 0.2,
+    spendMoney: 420000,
   },
   {
-    type: 'sell',
-    amount: 1655.1,
-    spendMoney: 149920,
-    rate: 90.58062957
-  }
+    type: 'buy',
+    rate: 1211329,
+    amount: 0.2,
+    spendMoney: 242266,
+  },
+  {
+    type: 'buy',
+    rate: 1158101,
+    amount: 0.2,
+    spendMoney: 231621,
+  },
 ]
 
-const calculateTotalAverage = (history) => {
-  return history.reduce((stack, next) => {
-    if (next.type === 'sell') return stack
+const bfHistory = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'bfTestData.json'), 'utf8')
+)
+const history = convertBfToCC(bfHistory).concat(unRegistedData)
 
-    stack.spendMoney += next.spendMoney
-    stack.amount += next.amount
-    stack.rate += next.rate
-    return stack
-  }, { amount: 0, rate: 0, spendMoney: 0 })
+const calculateTotalAverage = history => {
+  return history.reduce(
+    (stack, next) => {
+      if (next.type === 'sell') return stack
+
+      stack.spendMoney += next.spendMoney
+      stack.amount += next.amount
+      stack.rate += next.rate
+      return stack
+    },
+    { amount: 0, rate: 0, spendMoney: 0 }
+  )
 }
 
 const { amount, spendMoney, rate } = calculateTotalAverage(history)
