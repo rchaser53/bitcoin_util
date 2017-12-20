59132885e927177162b5107c7f1ef9d9b00d7b4c
rchaser53 <tayoshizawa29@gmail.com>
2017-12-20 22:38:44 +0900

    try to use averageCalculate
    maybe need to add tax at next

--- src/main.js
+++ src/main.js
@@ -1,26 +1,29 @@
 const fs = require('fs')
 const path = require('path')
+const math = require('mathjs')  
 
 const { convertBfToCC } = require('./calculateBf')
 
 const unRegistedData = [
   {
-    type: 'sell',
+    type: 'buy',
     rate: 2100000,
-    amount: 0.2,
-    spendMoney: 420000
+    amount: 0.000005,
+    tax: 0
   },
   {
     type: 'buy',
     rate: 1211329,
     amount: 0.2,
-    spendMoney: 242266
+    spendMoney: 242266,
+    tax: 0
   },
   {
     type: 'buy',
     rate: 1158101,
     amount: 0.2,
-    spendMoney: 231621
+    spendMoney: 231621,
+    tax: 0
   }
 ]
 
@@ -43,6 +46,50 @@ const calculateTotalAverage = history => {
   )
 }
 
-const { amount, spendMoney } = calculateTotalAverage(history)
+const calculateMovingAverage = history => {
+  const { amount, rate, spendMoney, type } = history.pop()
+  
 
-console.log(spendMoney / amount)
+  return history.reduce(
+    (stack, next) => {
+      const isSell = (next.type === 'sell')
+      if (isSell) {
+        
+
+        // console.log((stack.rate * next.amount) - (next.rate * next.amount))
+        // stack.spendMoney += next.spendMoney
+        // stack.rate = math.divide(stack.spendMoney, stack.amount)
+        stack.amount -= next.amount
+        stack.spendMoney += next.amount * stack.rate
+      } else {
+        // stack.amount += next.amount
+        // stack.spendMoney -= next.spendMoney
+        // stack.rate = math.divide(stack.spendMoney, stack.amount)
+
+        // const totalCost = stack.amount * stack.rate
+        // const nextCost =  next.amount * next.rate
+        // stack.rate = (nextCost + nextCost) / (stack.amount + next.amount)
+        // stack.amount += next.amount
+
+        stack.amount += next.amount
+        stack.spendMoney -= (next.amount * next.rate)
+        stack.rate = (stack.spendMoney / stack.amount)
+      }
+      return stack
+    },
+    {
+      amount: (type === 'sell') ? -1 * amount : amount,
+      spendMoney: (type === 'sell') ? spendMoney : -1 * spendMoney,
+      rate
+    }
+  )
+}
+
+// const { amount, spendMoney } = calculateTotalAverage(history)
+// console.log(history)
+console.log(calculateMovingAverage(history))
+
+// console.log(spendMoney / amount)
+
+
+// sum(amount * rate) / sum(amount)
