3b719068a8d51edc25223249b6765a673af18fe5
rchaser53 <t.yoshizawa@sorabito.com>
2017-12-20 14:27:25 +0900

    try to overwrite eslint

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "no-unused-vars":  ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
+    }
+};
